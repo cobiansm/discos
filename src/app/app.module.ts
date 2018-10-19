1ef4c30dc05e63a58cd3a72b63cc1208b0e5588c
Xenos141 <arturomr2001@hotmail.com>
2018-10-19 09:14:43 -0500

    imagen 1975

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Imagen1975Page } from '../pages/imagen1975/imagen1975';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    Imagen1975Page
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    Imagen1975Page
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Imagen1975Page } from '../imagen1975/imagen1975';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+imagen1 = Imagen1975Page;
 
   constructor(public navCtrl: NavController) {
 
+  }
+  imagen1975() {
+    this.navCtrl.push(this.imagen1);
   }
 
 }
